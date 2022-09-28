223d9fe29ca4d4c35a396959d1a23224a518fc95
mfontosco <59096300+mfontosco@users.noreply.github.com>
2022-09-28 20:52:17 +0100

    heroku practice

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 import morgan from 'morgan'
 import connectDB from './config/db.js';
 import BookReviewRouter from './routes/BookReviewRoute.js'
-
+import path from 'path'
 const app =express()
 
 app.use(express.json({limit:"50mb"}))
@@ -15,6 +15,14 @@ app.use('/api/v1/bookreview',BookReviewRouter)
 
 
 
+const _dirname =path.resolve()
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(_dirname,"/frontend/build")))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","build","index.html"))
+})
+}
 const start = async(Port)=>{
     try {
         const conn = await connectDB()
@@ -29,6 +37,6 @@ const start = async(Port)=>{
        console.log(err) 
     }
 }
-const Port = 8000
+const Port = process.env.Port || 8000
 start(Port)
 
