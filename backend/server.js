d0e6d80900afc0aa30a9a9620f5a6cf0b3f8d6fa
mfontosco <59096300+mfontosco@users.noreply.github.com>
2022-09-28 22:08:48 +0100

    added env

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,16 @@ app.use('/api/v1/bookreview',BookReviewRouter)
 
 
 
-const _dirname =path.resolve()
+const  __dirname = path.resolve()
 if(process.env.NODE_ENV === "production"){
-app.use(express.static(path.join(_dirname,"/frontend/build")))
+app.use(express.static(path.join(__dirname,"/frontend/build")))
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(_dirname,"frontend","build","index.html"))
+    res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
 })
 }
+
+
 const start = async(Port)=>{
     try {
         const conn = await connectDB()
